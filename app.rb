c81a0e4c4876d06c369fbd877f9c1158247b9c9a
Shaun Carland <shaun@Shauns-MacBook-Pro-2.local>
2020-03-19 20:02:53 -0400

    remove cruft

--- app.rb
+++ app.rb
@@ -1,27 +1,3 @@
 require 'sinatra'
 require "sinatra/reloader"
 require "pry"
-require_relative "person"
-
-before do
-    request.body.rewind
-    @request_payload = JSON.parse(request.body.read)
-end
-
-post '/v1/policy_prices' do
-    people = @request_payload["people"].map do |person|
-        Person.new(
-            gender: person["gender"].downcase,
-            age: person["age"],
-            name: person["name"],
-            health_condition: person["health condition"].downcase
-        )
-    end
-
-    policy_prices = people.map do |person|
-        { name: person.name, policy_price: person.policy_price }
-    end
-
-    content_type :json
-    { policy_prices: policy_prices }.to_json
-end

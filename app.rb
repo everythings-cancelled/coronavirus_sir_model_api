20d139ab596bd4320f1b4003086fffc56ed8baf4
Shaun Carland <shaun@Shauns-MacBook-Pro-2.local>
2020-03-19 21:29:04 -0400

    add empty classes

--- app.rb
+++ app.rb
@@ -1,3 +1,14 @@
 require 'sinatra'
 require "sinatra/reloader"
 require "pry"
+
+get "v1/sir_model" do
+    # who_api_adapter = WhoApiAdapter.new(who_api_url)
+    # sir_model_api_adapter = SirModelApiAdapter.new(sir_model_api_url)
+    # rest_countries_api_adapter = RestCountriesAdapter.new(rest_countries_api_url)
+    # coronavirus_tracker_api_adapter = CoronavirusTrackerApiAdapter.new(coronavirus_tracker_api_url)
+
+
+    # hospital_adapter = HospitalAdapter.new(hospital_url)
+    # region_adapter = RegionAdapter.new(region_url)
+end

001ea949822bc2aa972ec75df63d7d372ab3de1d
Shaun Carland <shauncarland@gmail.com>
2020-03-21 15:15:30 -0400

    Start using pombr covid19 gem

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ post "/v1/sir_model" do
     params = JSON.parse(request.body.read)
     country = Restcountry::Country.find_by_name(params["country"])
     who_api_adapter = WhoApiAdapter.new(country.alpha3Code)
-    covid19_latest_data = PomberCovid19.find_by_region_name(params["country"].capitalize).first
+    covid19_latest_data = PomberCovid19.find_by_region_name(params["country"].capitalize).last
 
     model = SirModel.new(
         eons: params["eons"].to_i,
